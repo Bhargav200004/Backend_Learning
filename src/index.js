02d28ed5eb0cc878733b2fdb3837c78f3d5136af
Bhargav <bhargav200004@gmail.com>
2025-05-01 15:54:00 +0530

    creating util functions apierror apiresponse asynchandler

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import dotEnv from 'dotenv';
 import connectDB from './db/dbConnect.js';
+import { app } from './app.js';
 
 dotEnv.config({
   path: './env',
 });
 
 
-connectDB();
+connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 8080 , () => {
+    console.log(`Server running on PORT ${process.env.PORT}`);
+  });
+})
+.catch((err) => {
+  console.log("MONGODB Connection failed: " , err);
+});
