f1d8ec8344aefcd7f8b9baa6229a241bdc42366c
Bhargav <bhargav200004@gmail.com>
2025-07-01 05:54:54 +0530

    feat: to implement odd checker

---
+++ src/__test__/utils/checkOdd.test.js
@@ -0,0 +1,10 @@
+import { test, expect } from 'vitest';
+import { IsOdd } from '../../utlis/checkOdd';
+
+test('check 10 is odd', () => {
+  expect(IsOdd(10)).toBe(false);
+});
+
+test('check 9 is odd', () => {
+  expect(IsOdd(9)).toBe(true);
+});
---
+++ src/utlis/checkOdd.js
@@ -0,0 +1,7 @@
+export function IsOdd(num) {
+  if (num % 2 !== 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
